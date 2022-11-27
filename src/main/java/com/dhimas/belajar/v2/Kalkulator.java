541bfa6776de7b55825fad14476163a6c3bbd180
hekilledhim <dhimzmarwahz@gmail.com>
2022-11-27 19:50:25 +0700

    first

---
+++ src/main/java/com/dhimas/belajar/v2/Kalkulator.java
@@ -0,0 +1,21 @@
+package com.dhimas.belajar.v2;
+
+public class Kalkulator {
+		
+		    public static int penjumlahan(int a, int b) {
+		        return a + b;
+		    }
+
+		    public static int pengurangan(int a, int b) {
+		        return a - b;
+		    }
+
+		    public static int pembagian(int a, int b) {
+		        return a / b;
+		    }
+
+		    public static int modulus(int a, int b) {
+		        return a % b;
+		    }
+	
+}
